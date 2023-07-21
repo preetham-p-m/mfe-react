69baca0e0361deea138edbb71c3303769129ecc5
preetham-coupa <preetham.p-m@coupa.com>
2023-07-21 14:43:13 +0530

    added header

--- container/src/App.js
+++ container/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <>
-      <h1>Hi Container</h1>
-      <hr />
+    <BrowserRouter>
+      <Header />
       <MarketingApp />
-    </>
+    </BrowserRouter>
   );
 };
 
